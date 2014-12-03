60d8c39f356e699bd6bf8413172a44727abc0c8a
Jakob Jensen <muhaar@gmail.com>
2014-12-03 14:31:51 +0100

    1) Name Fix. 2) Folders added.

--- LUNPO/obj/Debug/MainWindow.g.i.cs
+++ LUNPO/obj/Debug/MainWindow.g.i.cs
@@ -51,7 +51,7 @@ namespace LUNOP {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LUNOP;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LUNPO;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
