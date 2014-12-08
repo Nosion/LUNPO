4ea6bd6938fa13bad19d676fa7e1209cb71de211
Jakob Jensen <muhaar@gmail.com>
2014-12-08 14:24:35 +0100

    First plugin text example now working

--- FirstPlugin/FirstPlugin.cs
+++ FirstPlugin/FirstPlugin.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using PluginContracts;
 using Utils;
+using Microsoft.Win32;
 
 
 namespace FirstPlugin
 {
-  
+
+
 
         public class FirstPlugin : IPlugin
         {
@@ -26,16 +30,20 @@ namespace FirstPlugin
             {
                 get
                 {
-
+                    DelegateCommand<object> delegateSomething = new DelegateCommand<object>(Do);
                     var menu = new MenuItem("FirstPlugin");
-                    menu.Children.Add(new MenuItem("Write to console"));
+                    MenuItem menuItem = new MenuItem("Write to console");
                     
+                    
+                    menuItem.Command = delegateSomething;
+                    menu.Children.Add(menuItem);
                     return menu;
                 }
             }
 
-            public void Do()
+            public void Do(object obj)
             {
+               
               Console.WriteLine("Do Something in First Plugin");
             }
         }
