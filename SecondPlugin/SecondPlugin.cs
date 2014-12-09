3419406d80e04d2d4bee05c2882b2d4c0a8945f9
Mikkel <mikkel.aspendos@gmail.com>
2014-12-09 11:10:37 +0100

    Secondplugin unittest

--- SecondPlugin/SecondPlugin.cs
+++ SecondPlugin/SecondPlugin.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using PluginContracts;
 using Utils;
 
@@ -12,8 +14,10 @@ namespace SecondPlugin
 {
 
 
-    public class SecondPlugin : IPlugin
+    public class AutoSavePlugin : IPlugin
     {
+        private static double autosaveInterval;
+
         public string Name
         {
             get
@@ -27,14 +31,22 @@ namespace SecondPlugin
             get
             {
 
-                var menu = new MenuItem("SecondPlugin");
-                menu.Children.Add(new MenuItem("Write to console"));
+                var menu = new MenuItem("AutoSavePlugin");
+                menu.Children.Add(new MenuItem("Save to console"));
 
                 return menu;
             }
         }
 
-        public void Do()
+        DispatcherTimer autosaveTimer = new DispatcherTimer(TimeSpan.FromSeconds(autosaveInterval), DispatcherPriority.Background, new EventHandler(DoAutoSave), MediaTypeNames.Application.Current.Dispatcher);
+        
+
+        private void DoAutoSave(object sender, EventArgs e)
+        {
+            // Enter save logic here...
+        }
+
+        public void DoAutoSave()
         {
             Console.WriteLine("Do Something in Second Plugin");
         }
